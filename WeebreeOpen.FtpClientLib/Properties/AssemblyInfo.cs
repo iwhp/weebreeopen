fcccbd8d4c432b01e7b49f65b4ed4e14b21db9f5
Harry <harry@ardimedia.com>
2014-12-06 16:04:32 +0100

    Clean code.

--- WeebreeOpen.FtpClientLib/Properties/AssemblyInfo.cs
+++ WeebreeOpen.FtpClientLib/Properties/AssemblyInfo.cs
@@ -34,6 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.0.3.0")]
 [assembly: AssemblyFileVersion("0.0.3.0")]
-[assembly: InternalsVisibleTo("WeebreeOpen.FtpClientLib.Tests")]
-[assembly: InternalsVisibleTo("WeebreeOpen.FtpClientLib.Explorables")]
 
