7372d961346413e5d55b90fa0b7d9ec242f291d6
Harry Pfleger <harry@ardimedia.com>
2015-07-25 14:36:08 +0200

    Clean code.

--- WeebreeOpen.SharePointLib/Properties/AssemblyInfo.cs
+++ WeebreeOpen.SharePointLib/Properties/AssemblyInfo.cs
@@ -1,17 +1,15 @@
 ﻿using System.Resources;
 using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("WeebreeOpen.SharePointLib")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Release Date: 2015-07-25")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Weebree - Liechtenstein")]
 [assembly: AssemblyProduct("WeebreeOpen.SharePointLib")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © 2015 Weebree")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
@@ -19,12 +17,12 @@ using System.Runtime.InteropServices;
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
--- WeebreeOpenOffice365Api.Test/Properties/AssemblyInfo.cs
+++ WeebreeOpenOffice365Api.Test/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("WeebreeOpen.Office365Api.Test")]
-[assembly: AssemblyDescription("Release Date: 2014-11-08")]
+[assembly: AssemblyDescription("Release Date: 2015-07-25")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Weebree, Schaan - Liechtenstein")]
 [assembly: AssemblyProduct("WeebreeOpen.Office365Api.Test")]
-[assembly: AssemblyCopyright("Copyright © 2014 Weebree")]
+[assembly: AssemblyCopyright("Copyright © 2015 Weebree")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.1.0")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyVersion("0.0.1")]
+[assembly: AssemblyFileVersion("0.0.1")]
